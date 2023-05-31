833c8662402fde20a4260b24fa80d95da56949b3
Alexander Oropesa <alexorobtc@outlook.es>
2023-05-31 08:42:08 -0600

    Revert "New"
    This reverts commit 88d7ebd142c19dae42cf7a3dc5a71eada6d2ced4.

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 const log = console.log
+// Pongo todas las targetas en una constante 
+// para despues aplicarle un forEach
+const cards = document.querySelectorAll('.card')
 
-const next = document.getElementById('next')
-const back = document.getElementById('back')
-const circles = document.querySelectorAll('.circle')
-const line = document.querySelector('.line')
-
-//Variable de apollo para contador 
-let numRef = 1;
-
-//Listenir even al click en los botones
-next.addEventListener('click',()=>{
-  if(numRef<circles.length){
-        numRef++
-    }
-    activeCircle()
-})
-
-back.addEventListener('click',()=>{
-    if(numRef>1){
-        numRef--
-    }
-    activeCircle()
-})
-
-function activeCircle() {
-    circles.forEach((a,b)=>{
-        if (b<numRef){a.classList.add('active')}else{
-            a.classList.remove('active')
-        }
+// pongo un listener a todas las tarjetas para cuando pase el mouse
+// se le ponga a esa targeta la clase 'active'
+cards.forEach(item=>{
+    item.addEventListener('mouseover',()=>{
+        // llamo a la funcion clearClass q borra la clase 'active'
+        // a la targeta q lo tengo para asignarsela a la nueva tarjeta
+        // y no se repitan varias targetas con la misma clase
+        clearClass();
+        // asigno la clase active a la nueva targeta 
+        item.classList.add('active')
     })
-    const active = document.querySelectorAll('.active')
-    line.style.width = (active.length-1)/(circles.length-1)*100+'%'
+})
+
+// funcion clearClass q borra la clase 'active' a la antigua targeta
+function clearClass() {
+    cards.forEach(item=>{
+        item.classList.remove('active')
+    })
 }
